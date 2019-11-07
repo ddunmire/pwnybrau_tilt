482160e3522cc1ff9f44fccd0b3af7deabc1da16
Damon Dunmire <ddunmire@splunk.com>
2019-11-07 16:56:33 -0500

    added check on $SPLUNK_HOME.  if empty then "attempt" to figure out the path of the script. This was done to allow the script to be called from different locations.  for example from the CLI just to see what the tilts are reporting.

--- bin/pwnybrau_read_tilt_stdout.sh
+++ bin/pwnybrau_read_tilt_stdout.sh
@@ -12,4 +12,10 @@ export loglevel="ERROR"
 export listentime=8 
 
 # execute sevice
-python3 $SPLUNK_HOME/etc/apps/pwnybrau_tilt/bin/pwnybrau_read_tilt.py --listentime=$listentime --loglevel=$loglevel
+if [ -z ${SPLUNK_HOME} ]; then
+  DIRECTORY=$(cd `dirname $0` && pwd)
+  echo $DIRECTORY
+  python3 $DIRECTORY/pwnybrau_read_tilt.py --listentime=$listentime --loglevel=$loglevel
+else
+  python3 $SPLUNK_HOME/etc/apps/pwnybrau_tilt/bin/pwnybrau_read_tilt.py --listentime=$listentime --loglevel=$loglevel
+fi
